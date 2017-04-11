81f4b726947ef2a34eea2f50b2072af689791836
Animesh Pandey <animeshpandey.etno@gmail.com>
2017-04-11 00:17:18 -0400

    Changes output structure of plugin

--- src/main/java/org/elasticsearch/plugin/ingest/vader/processor/VaderProcessor.java
+++ src/main/java/org/elasticsearch/plugin/ingest/vader/processor/VaderProcessor.java
@@ -1,7 +1,6 @@
 package org.elasticsearch.plugin.ingest.vader.processor;
 
 import com.vader.sentiment.analyzer.SentimentAnalyzer;
-import com.vader.sentiment.util.Utils;
 import org.elasticsearch.ingest.AbstractProcessor;
 import org.elasticsearch.ingest.ConfigurationUtils;
 import org.elasticsearch.ingest.IngestDocument;
@@ -19,22 +18,14 @@ public final class VaderProcessor extends AbstractProcessor {
     public static final String TYPE = "vader_analyzer";
 
     private final String field;
-    private final String positivePolarityField;
-    private final String negativePolarityField;
-    private final String neutralPolarityField;
-    private final String compoundPolarityField;
+    private final String polarityField;
     private final SentimentAnalyzer sentimentAnalyzer;
     private final boolean ignoreMissing;
 
-    public VaderProcessor(String tag, String field, String positivePolarityField, String negativePolarityField,
-                          String neutralPolarityField, String compoundPolarityField, SentimentAnalyzer sentimentAnalyzer,
-                          boolean ignoreMissing) {
+    public VaderProcessor(String tag, String field, String polarityField, SentimentAnalyzer sentimentAnalyzer, boolean ignoreMissing) {
         super(tag);
         this.field = field;
-        this.positivePolarityField = positivePolarityField;
-        this.negativePolarityField = negativePolarityField;
-        this.neutralPolarityField = neutralPolarityField;
-        this.compoundPolarityField = compoundPolarityField;
+        this.polarityField = polarityField;
         this.sentimentAnalyzer = sentimentAnalyzer;
         this.ignoreMissing = ignoreMissing;
     }
@@ -43,20 +34,8 @@ public final class VaderProcessor extends AbstractProcessor {
         return field;
     }
 
-    public String getPositivePolarityField() {
-        return positivePolarityField;
-    }
-
-    public String getNegativePolarityField() {
-        return negativePolarityField;
-    }
-
-    public String getNeutralPolarityField() {
-        return neutralPolarityField;
-    }
-
-    public String getCompoundPolarityField() {
-        return compoundPolarityField;
+    public String getPolarityField() {
+        return polarityField;
     }
 
     boolean isIgnoreMissing() {
@@ -78,10 +57,8 @@ public final class VaderProcessor extends AbstractProcessor {
                 throw new IllegalArgumentException("field [" + field + "] doesn't exist");
             }
         }
-        validateTargetField(document, positivePolarityField);
-        validateTargetField(document, negativePolarityField);
-        validateTargetField(document, neutralPolarityField);
-        validateTargetField(document, compoundPolarityField);
+
+        validateTargetField(document, polarityField);
 
         Object value = document.getFieldValue(field, Object.class);
         if (value != null && value instanceof String) {
@@ -99,10 +76,7 @@ public final class VaderProcessor extends AbstractProcessor {
                         throw new IllegalArgumentException("field [" + field + "] should have a full name");
                     }
                     try {
-                        document.setFieldValue(positivePolarityField, polarity.get(Utils.POSITIVE_SCORE));
-                        document.setFieldValue(negativePolarityField, polarity.get(Utils.NEGATIVE_SCORE));
-                        document.setFieldValue(neutralPolarityField, polarity.get(Utils.NEUTRAL_SCORE));
-                        document.setFieldValue(compoundPolarityField, polarity.get(Utils.COMPOUND_SCORE));
+                        document.setFieldValue(polarityField, polarity);
                     } catch (Exception e) {
                         document.setFieldValue(field, value);
                         throw e;
@@ -128,14 +102,10 @@ public final class VaderProcessor extends AbstractProcessor {
 
         @Override
         public VaderProcessor create(Map<String, Processor.Factory> registry, String processorTag, Map<String, Object> config) throws Exception {
-            String field = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "field");
-            String positivePolarityField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "positive_polarity");
-            String negativePolarityField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "negative_polarity");
-            String neutralPolarityField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "neutral_polarity");
-            String compoundPolarityField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "compound_polarity");
+            String field = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "input_text_field");
+            String polarityField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "polarity_field");
             boolean ignoreMissing = ConfigurationUtils.readBooleanProperty(TYPE, processorTag, config, "ignore_missing", false);
-            return new VaderProcessor(processorTag, field, positivePolarityField, negativePolarityField,
-                    neutralPolarityField, compoundPolarityField, sentimentAnalyzer, ignoreMissing);
+            return new VaderProcessor(processorTag, field, polarityField, sentimentAnalyzer, ignoreMissing);
         }
     }
 }
